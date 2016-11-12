ccd2553ac3fa1cbbf7530e99b7be78f281a49c71
Iconmaster <crazybonesj@yahoo.com>
2016-11-12 12:51:27 -0500

    Initial commit of ADAM 2.

--- src/com/iconmaster/adam/equip/RepairResult.java
+++
@@ -1,9 +0,0 @@
-package com.iconmaster.adam.equip;
-
-/**
- *
- * @author iconmaster
- */
-public class RepairResult {
-	public double repaired;
-}
---
+++ src/main/java/info/iconmaster/adam/ADAM.java
@@ -0,0 +1,10 @@
+package info.iconmaster.adam;
+
+public class ADAM {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
