c066ae86d2a7fedf883ce4ce6c141333e7e9e606
Iconmaster <crazybonesj@yahoo.com>
2016-11-13 12:04:54 -0500

    Added a stub for a GUI!

--- src/main/java/info/iconmaster/adam/ADAM.java
+++ src/main/java/info/iconmaster/adam/ADAM.java
@@ -1,10 +1,12 @@
 package info.iconmaster.adam;
 
+import info.iconmaster.adam.gui.AdamGui;
+
 public class ADAM {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		AdamGui gui = new AdamGui();
+		gui.setVisible(true);
 	}
 	
 }
---
+++ src/main/java/info/iconmaster/adam/gui/AdamGui.java
@@ -0,0 +1,67 @@
+package info.iconmaster.adam.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+public class AdamGui extends JFrame {
+	private static final long serialVersionUID = -8286165289807880809L;
+	
+	public AdamGui() {
+		setSize(1024, 768);
+		setTitle("ADAM");
+		
+		JPanel topBar = new JPanel();
+		topBar.setLayout(new BoxLayout(topBar, BoxLayout.Y_AXIS));
+		
+		JPanel outputPanel = new JPanel(new BorderLayout());
+		outputPanel.setBorder(BorderFactory.createEtchedBorder());
+		JTextPane output = new JTextPane();
+		output.setEditable(false);
+		output.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+		outputPanel.add(output);
+		topBar.add(outputPanel);
+		
+		JTextField input = new JTextField();
+		input.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+		JPanel inputPanel = new JPanel(new BorderLayout()) {
+			private static final long serialVersionUID = -3145773988628965624L;
+			@Override
+			public Dimension getMaximumSize() {
+				return new Dimension(Integer.MAX_VALUE, input.getPreferredSize().height);
+			}
+		};
+		
+		JLabel inputLabel = new JLabel("  >  ");
+        inputPanel.add(inputLabel, BorderLayout.LINE_START);
+        inputPanel.add(input, BorderLayout.CENTER);
+        topBar.add(inputPanel);
+		
+		JPanel bottomBar = new JPanel();
+		bottomBar.setLayout(new BoxLayout(bottomBar, BoxLayout.X_AXIS));
+		
+		JPanel charPanel = new JPanel(new BorderLayout());
+		charPanel.setBorder(BorderFactory.createEtchedBorder());
+		bottomBar.add(charPanel);
+		
+		JPanel worldPanel = new JPanel(new BorderLayout());
+		worldPanel.setBorder(BorderFactory.createEtchedBorder());
+		bottomBar.add(worldPanel);
+		
+		JPanel inspectPanel = new JPanel(new BorderLayout());
+		inspectPanel.setBorder(BorderFactory.createEtchedBorder());
+		bottomBar.add(inspectPanel);
+		
+		add(topBar);
+		add(bottomBar);
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+	}
+}
