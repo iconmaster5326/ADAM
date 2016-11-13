eeded6cae21c21b72d49819f76f167e599b00972
Iconmaster <crazybonesj@yahoo.com>
2016-11-13 18:13:22 -0500

    Redamde the world view. Is better now.

--- src/main/java/info/iconmaster/adam/game/AdamGame.java
+++ src/main/java/info/iconmaster/adam/game/AdamGame.java
@@ -17,7 +17,7 @@ public class AdamGame {
 	public AdamGame() {
 		player = new BasicEntity("Bumpus", new Vector3(3,6,1), 200, Color.YELLOW);
 		BasicWorld earth = new BasicWorld("Earth", 1000000, 1000000);
-		earth.getChunk(new ChunkCoord(0, 0)).entities.put(player, new Vector3(5, 5, 5));
+		earth.getChunk(new ChunkCoord(0, 0)).entities.put(player, new Vector3(5, 0, 3));
 		controllable.add(player);
 	}
 	
--- src/main/java/info/iconmaster/adam/gui/WorldPane.java
+++ src/main/java/info/iconmaster/adam/gui/WorldPane.java
@@ -15,14 +15,67 @@ import info.iconmaster.adam.util.Vector3;
 public class WorldPane extends JPanel {
 	private static final long serialVersionUID = 8272220582389104958L;
 	
+	public static void drawWorldView(Graphics g, World world, Vector3 subject, int width, int height, int pxPerChunk) {
+		double chunksWide = Math.ceil((double) width / (double) pxPerChunk)+2; if (chunksWide%2==0) chunksWide++;
+		double chunksHigh = Math.ceil((double) height / (double) pxPerChunk)+2; if (chunksHigh%2==0) chunksHigh++;
+		
+		double realWidth = pxPerChunk*chunksWide;
+		double realHeight = pxPerChunk*chunksHigh;
+		
+		double chunkCenterX = subject.x / (double) World.VEC3_TO_CHUNK_SCALE;
+		double chunkCenterY = subject.z / (double) World.VEC3_TO_CHUNK_SCALE;
+		
+		double beginX = (width-realWidth)/2 - pxPerChunk*(chunkCenterX-Math.floor(chunkCenterX)) - pxPerChunk/2;
+		double beginY = (height-realHeight)/2 - pxPerChunk*(chunkCenterY-Math.floor(chunkCenterY)) - pxPerChunk/2;
+		
+		for (int x = 0; x < chunksWide; x++) {
+			for (int y = 0; y < chunksHigh; y++) {
+				Chunk ch = world.getChunk(new ChunkCoord(((int)(chunkCenterX-chunksWide/2))+x, ((int)(chunkCenterY-chunksHigh/2))+y));
+				
+				int x1 = (int) (beginX+x*pxPerChunk);
+				int y1 = (int) (beginY+y*pxPerChunk);
+				int x2 = pxPerChunk;
+				int y2 = pxPerChunk;
+				
+				g.setColor(new Color(Math.abs(ch.hashCode())%255, Math.abs(ch.hashCode())/255%255, Math.abs(ch.hashCode())/255/255%255));
+				g.fillRect(x1, y1, x2, y2);
+			}
+		}
+		
+		g.setColor(Color.RED);
+		for (int x = 0; x < chunksWide; x++) {
+			for (int y = 0; y < chunksHigh; y++) {
+				Chunk ch = world.getChunk(new ChunkCoord(((int)(chunkCenterX-chunksWide/2))+x, ((int)(chunkCenterY-chunksHigh/2))+y));
+				
+				for (Map.Entry<Entity, Vector3> entry : ch.entities.entrySet()) {
+					double px = entry.getValue().x % World.VEC3_TO_CHUNK_SCALE / World.VEC3_TO_CHUNK_SCALE;
+					double py = entry.getValue().z % World.VEC3_TO_CHUNK_SCALE / World.VEC3_TO_CHUNK_SCALE;
+					double pw = entry.getKey().getSize().x / World.VEC3_TO_CHUNK_SCALE;
+					double ph = entry.getKey().getSize().z / World.VEC3_TO_CHUNK_SCALE;
+					
+					int baseX = (int) (beginX+x*pxPerChunk)+pxPerChunk;
+					int baseY = (int) (beginY+y*pxPerChunk)+pxPerChunk;
+					
+					int x2 = (int) (pw*pxPerChunk);
+					int y2 = (int) (ph*pxPerChunk);
+					int x1 = (int) (baseX+px*pxPerChunk-x2/2);
+					int y1 = (int) (baseY+py*pxPerChunk-y2/2);
+					
+					g.drawRect(x1, y1, x2, y2);
+				}
+			}
+		}
+	}
+	
 	AdamGui adamGui;
-	int viewDistance = 5;
+	int viewDistance = 100;
 	
 	public WorldPane(AdamGui adamGui) {
 		this.adamGui = adamGui;
 		
 		addMouseWheelListener((ev)->{
 			viewDistance += ev.getUnitsToScroll()/ev.getScrollAmount();
+			if (viewDistance <= 0) viewDistance = 1;
 			
 			repaint();
 		});
@@ -37,33 +90,7 @@ public class WorldPane extends JPanel {
 			if (center == null) return;
 			World world = center.world;
 			
-			int xinc = getWidth()/viewDistance;
-			int yinc = getHeight()/viewDistance;
-			
-			for (int x = 0; x < viewDistance; x++) {
-				for (int y = 0; y < viewDistance; y++) {
-					Chunk ch = world.getChunk(new ChunkCoord(center.pos.x+x-viewDistance/2, center.pos.y+y-viewDistance/2));
-					
-					g.setColor(new Color(0, x*50 % 255, y*50 % 255));
-					g.fillRect(x*xinc, y*yinc, xinc, yinc);
-				}
-			}
-			
-			g.setColor(Color.RED);
-			for (int x = 0; x < viewDistance; x++) {
-				for (int y = 0; y < viewDistance; y++) {
-					Chunk ch = world.getChunk(new ChunkCoord(center.pos.x+x-viewDistance/2, center.pos.y+y-viewDistance/2));
-					
-					for (Map.Entry<Entity, Vector3> entry : ch.entities.entrySet()) {
-						double px = entry.getValue().x % World.VEC3_TO_CHUNK_SCALE / World.VEC3_TO_CHUNK_SCALE;
-						double py = entry.getValue().z % World.VEC3_TO_CHUNK_SCALE / World.VEC3_TO_CHUNK_SCALE;
-						double pw = entry.getKey().getSize().x / World.VEC3_TO_CHUNK_SCALE;
-						double ph = entry.getKey().getSize().z / World.VEC3_TO_CHUNK_SCALE;
-						
-						g.drawRect((int) (x*xinc+px*xinc), (int) (y*yinc+py*yinc), (int) (pw*xinc), (int) (ph*yinc));
-					}
-				}
-			}
+			drawWorldView(g, world, center.entities.get(adamGui.game.player), getWidth(), getHeight(), viewDistance);
 		}
 	}
 }
